b1d3d2461c2e7752564103e638b1e2298515f51a
fe-xiaoxin <zxin088@gmail.com>
2023-10-17 11:34:23 +0800

    ✨ feat(app): 再次调整 CI 流程

--- .release-it.cjs
+++ .release-it.cjs
@@ -2,12 +2,13 @@
 module.exports = {
   hooks: {
     'before:init': ['npm run typecheck', 'npm run test:ci'],
-    'after:bump': 'npm run build',
+    'after:bump': ['npx auto-changelog -p', 'npm run build'],
     'after:git:release': 'echo After git push, before github release',
     'after:release': 'echo Successfully released ${name} v${version} to ${repo.repository}.',
   },
   git: {
     commitMessage: 'chore: release v${version}',
+    changelog: 'npx auto-changelog --stdout --commit-limit false -u --template https://raw.githubusercontent.com/release-it/release-it/main/templates/changelog-compact.hbs',
   },
   github: {
     release: true,
